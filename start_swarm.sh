c5bfe1da74ea0c9eec8cb0669894f325f94e81a7
Maaarmotte <maaarmotte@gmail.com>
2019-08-18 14:24:10 +0100

    Added custom addon that is mounted at startup

--- start_swarm.sh
+++ start_swarm.sh
@@ -10,6 +10,7 @@ docker service create \
 	--mount type=volume,src=simple-gmod-server-addons,dst=/home/steam/gmod/garrysmod/addons \
 	--mount type=volume,src=simple-gmod-server-data,dst=/home/steam/gmod/garrysmod/data \
 	--mount type=volume,src=simple-gmod-server-cache-srcds,dst=/home/steam/gmod/garrysmod/cache/srcds \
+	--mount type=bind,src=$PWD/mounted_addon,dst=/home/steam/gmod/garrysmod/addons/mounted_addon \
 	--mount type=bind,src=$PWD/sv.db,dst=/home/steam/gmod/garrysmod/sv.db \
 	--mount type=bind,src=$PWD/cfg,dst=/home/steam/gmod/garrysmod/cfg \
 	simple-gmod-server:1.0.0
