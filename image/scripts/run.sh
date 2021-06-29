1117259055ddb728294064354b42ae82e12836b7
Loïc Habegger <habegger.loic@gmail.com>
2021-06-29 19:48:35 +0200

    Don't start with VAC enabled

--- image/scripts/run.sh
+++ image/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while (true); do
-	/home/steam/gmod/srcds_run -game garrysmod -port $PORT -authkey $(cat /home/steam/steam_api_key.txt) +maxplayers $MAXPLAYERS +host_workshop_collection $COLLECTIONID +map $DEFAULTMAP -ip 0.0.0.0
+	/home/steam/gmod/srcds_run -insecure -game garrysmod -port $PORT -authkey $(cat /home/steam/steam_api_key.txt) +maxplayers $MAXPLAYERS +host_workshop_collection $COLLECTIONID +map $DEFAULTMAP -ip 0.0.0.0
 	sleep 5;
 done;
