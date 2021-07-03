f4f5a056084120dae2e0cdf82dbda69717c164db
Maaarmotte <habegger.loic@gmail.com>
2021-07-03 14:31:08 +0000

    Added very limited script to pull addons without restart

---
+++ image/scripts/auto_pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clone_or_pull() {
+    git clone "$1" "./$2" || (cd "$2"; git pull origin master; cd "..")
+}
+
+cd /home/steam/gmod/garrysmod/addons
+
+while (true); do
+	nc -l -p 28015 -c 'echo "HTTP/1.1 200 OK\r\n\r\nOK"'
+
+	echo "Updating addons..."
+	clone_or_pull https://github.com/Maaarmotte/AirboatMod.git airboatmod;
+done
