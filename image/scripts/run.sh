e784459f17684d38c441a3c8fcd251c85af06488
Maaarmotte <habegger.loic@gmail.com>
2021-07-06 20:53:21 +0000

    Removed autopull

--- image/scripts/run.sh
+++ image/scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while (true); do
-	/home/steam/auto_pull.sh &
+	/home/steam/steamcmd.sh +login anonymous +force_install_dir gmod +app_update 4020 validate +quit
 	/home/steam/manual_addons.sh
-	/home/steam/gmod/srcds_run -insecure -game garrysmod -port $PORT -authkey $(cat /home/steam/steam_api_key.txt) +maxplayers $MAXPLAYERS +host_workshop_collection $COLLECTIONID +map $DEFAULTMAP -ip 0.0.0.0
+	/home/steam/gmod/srcds_run -insecure -game garrysmod -port $PORT -authkey $(cat /home/steam/steam_api_key.txt) +sv_setsteamaccount $GSLT +maxplayers $MAXPLAYERS +host_workshop_collection $COLLECTIONID +map $DEFAULTMAP -ip 0.0.0.0
 	sleep 5;
 done;
